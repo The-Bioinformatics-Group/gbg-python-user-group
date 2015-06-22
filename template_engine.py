415b62d678dc5b0167acbe703e88d5310d512d5a
Mats Töpel <mats@topel.se>
2015-06-22 21:50:25 +0200

    Code that quickly creates a template for a new python script

---
+++ template_engine.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+
+try:
+	import argparse
+except ImportError:
+	sys.stderr.write("[Error] The python module \"argparse\" is not installed\n")
+	sys.stderr.write("[--] Would you like to install it now using 'sudo easy_install' [Y/N]? ")
+	answer = sys.stdin.readline()
+	if answer[0].lower() == "y":
+		sys.stderr.write("[--] Running \"sudo easy_install argparse\"\n")
+		from subprocess import call
+		call(["sudo", "easy_install", "argparse"])
+	else:
+		sys.exit("[Error] Exiting due to missing dependency \"argparser\"")
+
+parser = argparse.ArgumentParser(prog="template_engine.py")
+parser.add_argument("name", nargs='?', help="Set the name for the script to create", default="new_script.py")
+args = parser.parse_args()
+
+
+def main():
+	output_file = open(args.name, "w")
+
+	output_file.write("""#!/usr/bin/env python
+
+import sys
+
+try:
+    import argparse
+except ImportError:
+	sys.stderr.write("[Error] The python module \'argparse\' is not installed\\n")
+	sys.stderr.write("[--] Would you like to install it now using 'sudo easy_install' [Y/N]? ")
+	answer = sys.stdin.readline()
+	if answer[0].lower() == "y":
+		sys.stderr.write("[--] Running \'sudo easy_install argparse\'\\n")
+		from subprocess import call
+		call(["sudo", "easy_install", "argparse"])
+	else:
+		sys.exit("[Error] Exiting due to missing dependency \'argparser\'")
+														        
+parser = argparse.ArgumentParser(prog="ADD-SCRIPT-NAME-HERE")
+parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
+args = parser.parse_args()
+
+def main():
+	### Remove the next line and add your own code instead ###
+	print "Try \'%s -h\'" % sys.argv[0]
+
+
+if __name__ == "__main__":
+    main()
+""")
+
+
+
+if __name__ == "__main__":
+	main()
